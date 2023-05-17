f176f15ff991f1a1f446d928f7375b602ec0aa42
surfer_liner <example@example.com>
2023-05-17 20:53:14 +0600

    'Editting in process'

--- convertor_txt_to_pdf.py
+++ convertor_txt_to_pdf.py
@@ -1,25 +1,44 @@
-import os
+import sys, os
 from io import BytesIO
 from pathlib import Path
 from reportlab.pdfgen import canvas
+
+sys.path.append('/Users/surfer_liner/python_/projects/useful_functions')
+# Find path to desktop
+desktop_path = str(Path.home() / 'Desktop')
+
+# ==============================================================================
+# Enter here .txt filename on desktop
+txt_filename = 'test.txt'
+# ==============================================================================
 
 
 def convert_txt_to_pdf(input_file):
     '''Simple convertor from .txt to .pdf'''
     try:
-        # Find path to desktop
-        desktop_path = str(Path.home() / 'Desktop')
         # Path to .txt file if it is on the desktop
-        path_to_input_file = desktop_path + '/' + input_file
+        path_to_input_file = desktop_path + '/' + txt_filename
+
         # Create buffer
         buffer = BytesIO()
         # Create PDF Canvas
         pdf = canvas.Canvas(buffer)
-        # Open .txt file and read content
+        # Open .txt file and read context
+
         with open(path_to_input_file, 'r') as f:
-            txt_file_content = f.read()
-        # Add text to PDF Canvas
-        pdf.drawString(50, 780, txt_file_content)
+            txt_file_content = f.readlines()
+            # Save context as string
+            file_content = ' '.join(txt_file_content)
+
+        # Weight of PDF page
+        string_len = 87
+        # Numbers of strings to PDF file
+        strings = round(len(''.join(txt_file_content)) / string_len)
+
+
+
+
+
         # Close and save PDF Canvas
         pdf.save()
         # Create path to new PDF
@@ -31,5 +50,4 @@ def convert_txt_to_pdf(input_file):
         print(f'File <{input_file}> not found\nCheck file name and try again')
 
 
-# Enter here .txt filename on desktop
-convert_txt_to_pdf('test.txt')
+convert_txt_to_pdf(txt_filename)
